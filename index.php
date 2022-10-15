1b339a827e0e6016d32ee50575d5a0a1f8b27572
Barrah Collins <68109115+bcollynz@users.noreply.github.com>
2022-10-15 12:36:10 +0100

    user data

--- index.php
+++ index.php
@@ -10,17 +10,17 @@
 
 <body>
       <form  method="POST" action="user_data.php">
-        <label for ="name"> Name: <input placeholder="Full Name" type="text" name="name" required></label><br>
-        <label for ="email"> Email: <input placeholder="Email" type="email" name="email" required></label><br>
+        <label for ="name"> Name: <input placeholder="Full Name" type="text" name="name" required></label><br><br>
+        <label for ="email"> Email: <input placeholder="Email" type="email" name="email" required></label><br><br>
+        <label for="birthday">Date of Birth:</label>
+            <input type="date" id="birthday" name="birthday"><br><br>
         <label for="gender">Gender:</label>
             <Select name="gender" id="gender" required>
               <option value="select">Select Gender</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
             </Select><br><br>
-          <label for="birthday">Date of Birth:</label>
-            <input type="date" id="birthday" name="birthday">
-          </div><br>
+            <label for ="country"> Country: <input placeholder="Country" type="text" name="country" required></label><br>
 
         <button type="submit">Submit</button>
 
--- user_data.php
+++ user_data.php
@@ -1,3 +1,13 @@
 <?php
-print_r($_POST);
+$name = $_POST ['name'];
+$email = $_POST ['email'];
+$birthday = $_POST ['birthday'];
+$gender = $_POST ['gender'];
+$country = $_POST ['country'];
+
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email . "<br>";
+echo "Birthday: " . $birthday . "<br>";
+echo "Gender: " . $gender . "<br>";
+echo "Country: " . $country . "<br>";
 ?>
