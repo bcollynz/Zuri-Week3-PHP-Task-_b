0e85649d657922ca2705b37909f59e8139b4e719
Barrah Collins <68109115+bcollynz@users.noreply.github.com>
2022-10-15 11:50:25 +0100

    Form POST

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <form>
+      <form action="user_data.php" method="POST">
+        <label for ="name"> Name: <input placeholder="Full Name" type="text" name="name" required></label><br>
+        <label for ="email"> Email: <input placeholder="Email" type="email" name="email" required></label><br>
+        <label for="gender">Gender:</label>
+            <Select name="gender" id="gender" required>
+              <option value="select">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </Select><br><br>
+          <label for="birthday">Date of Birth:</label>
+            <input type="date" id="birthday" name="birthday">
+          </div><br>
+
+        <button type="submit">Submit</button>
+
+</form>
