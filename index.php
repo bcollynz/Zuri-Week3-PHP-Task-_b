0189793582ffff4b02311a0a31241d0300cf14f4
Barrah Collins <68109115+bcollynz@users.noreply.github.com>
2022-10-15 12:07:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Form</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    
 </head>
 
 <body>
   <form>
-      <form  method="POST" action="user_data.php" >
+      <form  method="POST" action="user_data.php">
         <label for ="name"> Name: <input placeholder="Full Name" type="text" name="name" required></label><br>
         <label for ="email"> Email: <input placeholder="Email" type="email" name="email" required></label><br>
         <label for="gender">Gender:</label>
